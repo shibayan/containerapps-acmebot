08185712c0f087a7ffe839718d2ab406fbf8b0e5
Tatsuro Shibamura <me@shibayan.jp>
2022-07-24 22:41:48 +0900

    Cleanup dead code

--- ContainerApps.Acmebot/Internal/AzureSdkExtensions.cs
+++ ContainerApps.Acmebot/Internal/AzureSdkExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-using Azure.ResourceManager.Applications.Containers;
 using Azure.ResourceManager.Dns;
 using Azure.ResourceManager.Resources;
 
@@ -9,48 +8,6 @@ namespace ContainerApps.Acmebot.Internal;
 
 internal static class AzureSdkExtensions
 {
-    public static async Task<IReadOnlyList<ManagedEnvironmentData>> ListAllManagedEnvironmentsAsync(this SubscriptionResource subscription)
-    {
-        var managedEnvironments = new List<ManagedEnvironmentData>();
-
-        var result = subscription.GetManagedEnvironmentsAsync();
-
-        await foreach (var managedEnvironment in result)
-        {
-            managedEnvironments.Add(managedEnvironment.Data);
-        }
-
-        return managedEnvironments;
-    }
-
-    public static async Task<IReadOnlyList<ContainerAppData>> ListAllContainerAppsAsync(this SubscriptionResource subscription)
-    {
-        var containerApps = new List<ContainerAppData>();
-
-        var result = subscription.GetContainerAppsAsync();
-
-        await foreach (var containerApp in result)
-        {
-            containerApps.Add(containerApp.Data);
-        }
-
-        return containerApps;
-    }
-
-    public static async Task<IReadOnlyList<ContainerAppCertificateData>> ListAllAsync(this ContainerAppCertificateCollection collection)
-    {
-        var containerAppCertificates = new List<ContainerAppCertificateData>();
-
-        var result = collection.GetAllAsync();
-
-        await foreach (var containerAppCertificate in result)
-        {
-            containerAppCertificates.Add(containerAppCertificate.Data);
-        }
-
-        return containerAppCertificates;
-    }
-
     public static async Task<IReadOnlyList<DnsZoneResource>> ListAllDnsZonesAsync(this SubscriptionResource subscription)
     {
         var dnsZones = new List<DnsZoneResource>();
